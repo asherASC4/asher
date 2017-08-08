e3883ceb018a6a62f73b5eb58f6f6b3ed68bb4cf
asherASC4 <asher232435@gmail.com>
2017-08-08 16:00:18 -0400

    websites

--- rockps/sketch.js
+++ rockps/sketch.js
@@ -1,54 +1,70 @@
-function myFunction() {
-    var userChoice = prompt(" rock, paper or scissors?").toLowerCase();
+var score = 0;
+var games = 0;
+var userChoice = "";
+var computerChoice = 0;
 
-var computerChoice = Math.random();
-
-if (computerChoice < 0.34) {
- computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
- computerChoice = "paper";
-} else {
- computerChoice = "scissors";
+function printer(msg) {
+  document.getElementById('result').innerHTML = msg;
+  document.getElementById('score').innerHTML = score + "/" + games;
 }
 
-document.write("Computer chose: " + computerChoice);
-document.write("<br>");
-document.write("You chose: " + userChoice);
-document.write("<br>");
-
-var compare = function(choice1, choice2) {
-    if (choice1 === choice2) {
-        return "You tied! try again.";
-        prompt("Do you choose rock, paper or scissors?").toLowerCase();
-    }
-    else if (choice1 === "rock") {
-        if(choice2 === "scissors") {
-            return "rock wins";
-        }
-        else {
-            return "paper wins";
-        }  
-    }
-    else if (choice1 === "paper") {
-        if(choice2 === "rock") {
-            return "paper wins";
-        }
-        else {
-            return "scissors win";
-        }
-    }
-    else if (choice1 === "scissors") {
-        if (choice2 === "rock") {
-            return "rock wins";
-        }
-        else {
-            return "scissors win";
-        }
-    }
-   else {
-       return "I'm sorry that is not a valid answer."
-   }
-    };
-    document.write(compare(userChoice, computerChoice));
+// reseting
+document.getElementById('reset').onclick = function() {
+  score = 0;
+  games = 0;
+  printer("&nbsp;");
 }
 
+// handling user's choice
+function userChose(choice){
+    userChoice = choice;
+     compare(userChoice, compChoose());
+}
+
+// handling computer's choice
+function compChoose() {
+  computerChoice = Math.random();
+  if (computerChoice < 0.34) {
+    computerChoice = "rock";
+  } else if (computerChoice < 0.67) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissor";
+  }
+  return computerChoice;
+}
+
+// comparison
+// parameters have to be in "you, me" order!
+function compare(user, comp) {
+  var result = "";
+  if (user === comp) {
+    // not sure how a tie should affect the score so it just doesn't ¯\_(ツ)_/¯
+    result = "tie";
+  } else if (user === "rock") {
+    games++;
+    if (comp === "paper") {
+      result = "i chose paper. you lose."
+    } else {
+      result = "i chose scissor. you win."
+      score++;
+    }
+  } else if (user === "paper") {
+    games++;
+    if (comp === "scissor") {
+      result = "i chose scissor. you lose."
+    } else {
+      result = "i chose rock. you win."
+      score++;
+    }
+  } else {
+    games++;
+    if (comp === "rock") {
+      result = "i chose rock. you lose."
+    } else {
+      result = "i chose paper. you win."
+      score++;
+    }
+  }
+  printer(result);
+}
